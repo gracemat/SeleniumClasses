065e90a7506729e649a7a026fb99c52ebcf11555
realgreeshma <greeshmahere@gmail.com>
2019-07-03 14:27:08 -0500

    Launched Browser for Skyscanner webpage

---
+++ src/main/java/com/seleniumsessions/CalendarClass.java
@@ -0,0 +1,15 @@
+package com.seleniumsessions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CalendarClass {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\ChromeDriver.exe");
+	WebDriver driver = new ChromeDriver();
+	
+	driver.get("https://www.skyscanner.co.in");
+	
+	
+}
+}
