5e45d69d5505783cc1a5857907f08481577c7b69
realgreeshma <greeshmahere@gmail.com>
2019-07-11 19:34:11 -0500

    Use Assert class methods instead of if-else methods

--- src/test/java/PageTest/LoginPageTest.java
+++ src/test/java/PageTest/LoginPageTest.java
@@ -1,9 +1,12 @@
 package PageTest;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,6 +19,29 @@ public class LoginPageTest {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.get("https://access.trivago.com/oauth/en-US/login?step=register");
+	} 
+	
+	@Test
+	public void loginPageTitleTest()
+	{
+		String loginTitle = driver.getTitle();
+		System.out.println("Home Page Title is :: " +loginTitle);
+		Assert.assertEquals(loginTitle, "Log in or create your account - trivago");
+//		if(loginTitle.equals("Log in or create your account - "))//when the test failed Testng doesnot show red here if we use if else methods
+//		{
+//			System.out.println("Test Passed");
+//		}else 
+//		{
+//			System.out.println("FAIL");
+//		}
+	}
+	
+	@Test
+	public void registerButtonTest()
+	{
+		boolean regFlag = driver.findElement(By.id("register_email_submit")).isDisplayed();
+		Assert.assertTrue(regFlag);
+		System.out.println(regFlag);
 	}
 	
 	@AfterTest
